41654b4acc4d382ed157c1cb6460418854df5bf2
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-23 12:56:11 +0530

    Adding react router

---
+++ vidly/src/components/notFound.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() { 
+    return (  <h1>Not Found</h1>);
+  }
+ 
+export default NotFound;<h1>Not Found</h1>
--- vidly/src/index.js
+++ vidly/src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
