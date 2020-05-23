f9c52ce23f2521087651a51b36b10013eb997f62
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-23 19:49:21 +0530

    Linking the movie form

--- vidly/src/components/movieForm.jsx
+++ vidly/src/components/movieForm.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
-class MovieForm extends Component {
-  state = {};
-  render() {
-    return <h1>Movie Form </h1>;
-  }
-}
+const MovieForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>Movie Form : {match.params.id}</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => history.push("/movies")}
+      >
+        Save
+      </button>
+    </div>
+  );
+};
 
 export default MovieForm;
