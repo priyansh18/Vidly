c1b5c6c7f7ef9592fc03f0da880d630e29bac04b
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-24 18:02:57 +0530

    RegisterForm

--- vidly/src/components/registerForm.jsx
+++ vidly/src/components/registerForm.jsx
@@ -2,15 +2,16 @@ import React, { Component } from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
 
-class LoginForm extends Form {
+class RegisterForm extends Form {
   state = {
-    data: { username: "", password: "" },
+    data: { username: "", email: "", password: "" },
     errors: {},
   };
 
   schema = {
     username: Joi.string().required().label("Username"),
-    password: Joi.string().required().label("Password"),
+    email: Joi.string().email().required().label("Email"),
+    password: Joi.string().required().min(5).label("Password"),
   };
 
   doSubmit = () => {
@@ -20,15 +21,16 @@ class LoginForm extends Form {
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <h1>Register</h1>
         <form onSubmit={this.handleSubmit}>
+          {this.renderInput("email", "Email", "email")}
           {this.renderInput("username", "Username")}
           {this.renderInput("password", "Password", "password")}
-          {this.renderButton("Login")}
+          {this.renderButton("Register")}
         </form>
       </div>
     );
   }
 }
 
-export default LoginForm;
+export default RegisterForm;
