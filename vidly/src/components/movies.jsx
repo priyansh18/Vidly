4f9d17ba7412c26444e17cb70e365ac69bfd88ed
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-26 22:36:19 +0530

    Replacing FakeGenreService

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
+import { getGenres } from "../services/genreService";
 import MoviesTable from "./moviesTable";
 import ListGroup from "./common/listGroup";
 import { Link } from "react-router-dom";
@@ -20,8 +20,10 @@ class Movies extends Component {
     sortColumn: { path: "title", order: "asc" },
   };
 
-  componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
+  async componentDidMount() {
+    // To spread operstor we need to convert into data
+    const { data } = await getGenres();
+    const genres = [{ name: "All Genres" }, ...data];
     this.setState({
       movies: getMovies(),
       genres,
