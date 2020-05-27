06a68eed69c15f6741023c743682d68e57baa416
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-05-27 15:09:07 +0530

    Logging in the user on registration

--- vidly/src/components/registerForm.jsx
+++ vidly/src/components/registerForm.jsx
@@ -18,7 +18,10 @@ class RegisterForm extends Form {
   doSubmit = async () => {
     // console.log("Submitted");
     try {
-      await register(this.state.data);
+      const response = await register(this.state.data);
+      console.log(response);
+      localStorage.setItem("token", response.headers["x-auth-token"]);
+      this.props.history.push("/");
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
